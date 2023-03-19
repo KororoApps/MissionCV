65fb4e5d750b348ea14aed661fe338546479d87c
KororoApps <elodie.sponton@yahoo.fr>
2023-03-19 18:03:27 +0100

    Centrage des planètes à l'ouverture de la worldBard

--- js-index.js
+++ js-index.js
@@ -50,6 +50,8 @@ const ELEMENTS = [
 
 const galaxy = document.getElementById("galaxy");
 const worldBar = document.getElementById("world-bar");
+let translateGalaxyX;
+let translateGalaxyY;
 
 ELEMENTS.forEach((element) => {
   const pictureSrc = element.planetSrc;
@@ -57,11 +59,24 @@ ELEMENTS.forEach((element) => {
   const pictureId = element.id;
   const createPlanet = document.createElement("img");
   galaxy.appendChild(createPlanet);
-  createPlanet.classList.add("planets");
+  createPlanet.classList.add("planet");
   createPlanet.src = pictureSrc;
   createPlanet.alt = pictureAlt;
   createPlanet.id = pictureId;
-  createPlanet.addEventListener("click", () => {
+  createPlanet.addEventListener("click", (e) => {
+    console.log(createPlanet.width / 2);
     worldBar.classList.toggle("active");
+    createPlanet.classList.toggle("active");
+
+    const centerX = (window.innerWidth - worldBar.offsetWidth) / 2;
+    const centerY = window.innerHeight / 2;
+
+    translateGalaxyX =
+      centerX - (createPlanet.offsetLeft + createPlanet.offsetWidth / 2);
+    translateGalaxyY =
+      centerY - (createPlanet.offsetTop + createPlanet.offsetHeight / 2);
+    galaxy.classList.toggle("active");
+    galaxy.style.setProperty("--translate-x", translateGalaxyX + "px");
+    galaxy.style.setProperty("--translate-y", translateGalaxyY + "px");
   });
 });
