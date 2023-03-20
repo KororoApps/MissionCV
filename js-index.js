646f6b728c44b83656d93a5395b0f2a12b8b42de
KororoApps <elodie.sponton@yahoo.fr>
2023-03-20 21:11:05 +0100

    Ajout et animation des targets

--- js-index.js
+++ js-index.js
@@ -2,49 +2,110 @@ const ELEMENTS = [
   {
     id: "intro-planet",
     planetSrc: ["./Cv_Galaxy/map/intro_planet.png"],
-    alt: "planète_Terre",
+    planetAlt: "Planète Terre",
+    height: "22vh",
+    top: "40%",
+    left: "2%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-16%",
+    translateTargetY: "-15%",
   },
   {
     id: "mando-planet",
     planetSrc: "./Cv_Galaxy/map/mando_planet.png",
     backgroundSrc: "./Cv_Galaxy/background_worlds/mando.png",
     parchmentSrc: "./Cv_Galaxy/background_worlds/mando_cv.png",
-    alt: "planète_Mandalorian",
+    planetAlt: "Planète Mandalorian",
+    backgroundAlt: "Mando et Grogu",
+    parchmentAlt: "CV Parchemin",
+    height: "20vh",
+    top: "15%",
+    left: "30%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-20%",
+    translateTargetY: "-20%",
   },
   {
     id: "mononoke-planet",
     planetSrc: "./Cv_Galaxy/map/mononoke_planet.png",
     backgroundSrc: "./Cv_Galaxy/background_worlds/mononoke.png",
     parchmentSrc: "./Cv_Galaxy/background_worlds/mononoke_cv.png",
-    alt: "planète_MononokeHime",
+    planetAlt: "Planète Princesse Mononoké",
+    backgroundAlt: "Dans la forêt",
+    parchmentAlt: "CV Parchemin",
+    height: "25vh",
+    top: "10%",
+    left: "50%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-10%",
+    translateTargetY: "-15%",
   },
   {
     id: "porco-planet",
     planetSrc: "./Cv_Galaxy/map/porco_planet.png",
     backgroundSrc: "./Cv_Galaxy/background_worlds/porco.png",
     parchmentSrc: "./Cv_Galaxy/background_worlds/porco_cv.png",
-    alt: "planète_PorcoRocco",
+    planetAlt: "Planète Porco Rosso",
+    backgroundAlt: "Luffy aviatrice",
+    parchmentAlt: "CV Parchemin",
+    height: "20vh",
+    top: "15%",
+    left: "75%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-24%",
+    translateTargetY: "-20%",
   },
   {
     id: "catsby-planet",
     planetSrc: "./Cv_Galaxy/map/catsby_planet.png",
     backgroundSrc: "./Cv_Galaxy/background_worlds/catsby.png",
     parchmentSrc: "./Cv_Galaxy/background_worlds/catsby_cv.png",
-    alt: "planète_Catsby",
+    planetAlt: "Planète Catsby",
+    backgroundAlt: "Chat Catsby",
+    parchmentAlt: "CV Parchemin",
+    height: "25vh",
+    top: "70%",
+    left: "20%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-13%",
+    translateTargetY: "-10%",
   },
   {
     id: "simpson-planet",
     planetSrc: "./Cv_Galaxy/map/simpson_planet.png",
     backgroundSrc: "./Cv_Galaxy/background_worlds/simpson.png",
     parchmentSrc: "./Cv_Galaxy/background_worlds/simpson_cv.png",
-    alt: "planète_Simpson",
+    planetAlt: "Planète Simpson",
+    backgroundAlt: "Petit papa Noël devant la télé",
+    parchmentAlt: "CV Parchemin",
+    height: "20vh",
+    top: "75%",
+    left: "45%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-23%",
+    translateTargetY: "-20%",
   },
   {
     id: "ange-planet",
     planetSrc: "./Cv_Galaxy/map/ange_planet.png",
     backgroundSrc: "./Cv_Galaxy/background_worlds/ange.png",
     parchmentSrc: "./Cv_Galaxy/background_worlds/ange_cv.png",
-    alt: "planète_ange",
+    planetAlt: "Planète tableau",
+    backgroundAlt: "Un ange",
+    parchmentAlt: "CV Parchemin",
+    height: "25vh",
+    top: "60%",
+    left: "70%",
+    targetWidth: "33vh",
+    targetHeight: "33vh",
+    translateTargetX: "-15%",
+    translateTargetY: "-10%",
   },
 ];
 
@@ -54,27 +115,61 @@ let translateGalaxyX;
 let translateGalaxyY;
 
 ELEMENTS.forEach((element) => {
-  const pictureSrc = element.planetSrc;
-  const pictureAlt = element.alt;
-  const pictureId = element.id;
-  const createPlanet = document.createElement("img");
-  galaxy.appendChild(createPlanet);
-  createPlanet.classList.add("planet");
-  createPlanet.src = pictureSrc;
-  createPlanet.alt = pictureAlt;
-  createPlanet.id = pictureId;
-  createPlanet.addEventListener("click", (e) => {
-    console.log(createPlanet.width / 2);
-    worldBar.classList.toggle("active");
-    createPlanet.classList.toggle("active");
+  //CREATION DE LA DIV ACCUEILLANT LA PLANETE, LA TARGET, L'IMAGE ET LE CV
+  const containerPlanet = document.createElement("div");
+  galaxy.appendChild(containerPlanet);
+  containerPlanet.classList.add("containerPlanet");
+  containerPlanet.style.top = element.top;
+  containerPlanet.style.left = element.left;
+  containerPlanet.style.setProperty("--target-width", element.targetWidth);
+  containerPlanet.style.setProperty("--target-height", element.targetHeight);
+  containerPlanet.style.setProperty(
+    "--target-translate-x",
+    element.translateTargetX
+  );
+  containerPlanet.style.setProperty(
+    "--target-translate-y",
+    element.translateTargetY
+  );
 
+  //CREATION DE LA BALISE ACCUEILLANT L'IMAGE DE LA PLANETE
+  const createPlanet = document.createElement("img");
+  containerPlanet.appendChild(createPlanet);
+  createPlanet.classList.add("planet");
+  createPlanet.style.height = element.height;
+  createPlanet.src = element.planetSrc;
+  createPlanet.alt = element.planetAlt;
+
+  //AU CLIC SUR LA PLANETE...
+  containerPlanet.addEventListener("click", () => {
+    //DEPLOYER LA WORLDBAR
+    worldBar.classList.toggle("active");
+
+    //CREER DANS LA WORLDBAR LA BALISE POUR LE BACKGROUND ET LA BALISE POUR LE CV
+    const backgroundWorld = document.createElement("img");
+    const parchmentWorld = document.createElement("img");
+    worldBar.appendChild(backgroundWorld);
+    worldBar.appendChild(parchmentWorld);
+
+    //IMAGE DE FOND DE LA PLANETE
+    backgroundWorld.src = element.backgroundSrc;
+    console.log(backgroundWorld);
+
+    //IMAGE DU CV
+    parchmentWorld.src = element.parchmentSrc;
+
+    //GROSSIR LA DIV DE LA PLANETE
+    containerPlanet.classList.toggle("active");
+
+    //CENTRER LA DIV DE LA PLANETE SUR L'ECRAN RESTANT
     const centerX = (window.innerWidth - worldBar.offsetWidth) / 2;
     const centerY = window.innerHeight / 2;
 
     translateGalaxyX =
-      centerX - (createPlanet.offsetLeft + createPlanet.offsetWidth / 2);
+      centerX - (containerPlanet.offsetLeft + containerPlanet.offsetWidth / 2);
     translateGalaxyY =
-      centerY - (createPlanet.offsetTop + createPlanet.offsetHeight / 2);
+      centerY - (containerPlanet.offsetTop + containerPlanet.offsetHeight / 2);
+
     galaxy.classList.toggle("active");
     galaxy.style.setProperty("--translate-x", translateGalaxyX + "px");
     galaxy.style.setProperty("--translate-y", translateGalaxyY + "px");
