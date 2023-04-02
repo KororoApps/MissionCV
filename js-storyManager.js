023ee47a206344d9499a26a5324eedc0269aa3f5
KororoApps <elodie.sponton@yahoo.fr>
2023-04-02 12:49:56 +0200

    Exportation de l'array ELEMENTS et de l'array STORY

---
+++ js-storyManager.js
@@ -0,0 +1,16 @@
+export const STORY = [
+  {
+    image: "./Cv_Galaxy/story/story_01.png",
+    txt: `Haaaaa ! Mon CV ! <br> Noooon, revenez !!`,
+  },
+  {
+    image: "./Cv_Galaxy/story/story_02.png",
+    txt: `Les chats de l'espace ont encore sévi ! <br>
+  Ils volent les CV afin que les humains ne trouvent pas de travail et passent leur temps chez eux avec leur chat !`,
+  },
+  {
+    image: "./Cv_Galaxy/story/story_03.png",
+    txt: `Est-ce que voulez bien m'aider à retrouver mon CV ? <br>
+  Je crois qu'ils l'ont dispersé dans le multivers. <br> J'ai seulement pu retrouver ce morceau...`,
+  },
+];
