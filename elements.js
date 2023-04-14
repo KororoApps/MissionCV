b371ccb41ec25d3a18a43e69a256a3789803d0f1
KororoApps <elodie.sponton@yahoo.fr>
2023-04-14 08:13:57 +0200

    Modification statut développeur et suppression numéro de téléphone

--- elements.js
+++ elements.js
@@ -19,11 +19,11 @@ export const ELEMENTS = [
     rightPosition: "10%",
     topPosition: "50%",
     pieceOfCV: `<h1>ELODIE SPONTON</h1> 
-      <h4>DEVELOPPEUSE WEB</h4>
+      <h4>DEVELOPPEUSE INFORMATIQUE</h4>
       <h2>COORDONNEES</h2>
-      <p><span>Tél</span> : <a href="tel:0642965226">06 42 96 52 26</a></p>
+      <p><span>Tél</span> : N'hésitez pas à me le demander :)</p>
       <p><span>Mail</span>: <a href="mailto:elodie.sponton@yahoo.fr">elodie.sponton@yahoo.fr</a></p>
-      <p><span>Adresse</span> : 4H allée de la 1er Division Fr Libre, 21000 DIJON</p>`,
+      <p><span>Adresse</span> : DIJON</p>`,
     cvRetrieve: true,
   },
   {
